48d6db3e6dcbe4b3dda8bd86663c2381a6b329f1
nirjona <nirjonafariha@gmail.com>
2022-06-11 10:37:53 +0600

    push again

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, send_from_directory, Response
-from database import client
+
 
 app = Flask(__name__, static_url_path='')
 
-print(client)
+
 
 @app.route('/<path:path>')
 def send_static(path):
@@ -23,7 +23,7 @@ def area_map():
     return render_template('area_map.html')
 @app.route('/history')
 def history():
-    return render_template('history.html', client=client)
+    return render_template('history.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
