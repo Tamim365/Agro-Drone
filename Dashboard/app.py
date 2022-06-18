376328e0f79e34e740756ccf337c80d732126612
Tamim365 <tamim.365.ti@gmail.com>
2022-06-19 00:16:36 +0600

    login register

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, send_from_directory, Response
-from database import client
+from flask import Flask, Request, render_template, send_from_directory, Response, redirect
+# from database import client
+import sqlite3
+
+from requests import request
+from model import user
 
 app = Flask(__name__, static_url_path='')
+conn = sqlite3.connect('./agro_drone.db')
 
-print(client)
 
 @app.route('/<path:path>')
 def send_static(path):
@@ -23,7 +27,21 @@ def area_map():
     return render_template('area_map.html')
 @app.route('/history')
 def history():
-    return render_template('history.html', client=client)
+    return render_template('history.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/registerUser', methods=["POST", "GET"])
+def registerUser():
+    name = Request.form['name']
+    # email = request.form['email'] 
+    # password = request.form['password']
+    print(name)
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
